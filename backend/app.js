28145e27f312f1c0fe101029dad6d3967cc74f69
Clement Jeulin <clement93jeulin@gmail.com>
2022-12-18 10:31:37 +0100

    Code Cleaning

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,6 @@ const app = express();
 
 //IMPORT DE : module CORS, permettant d'éviter les erreurs relatives à l'origine de la requête 
 const cors = require('cors');
-//Lorsque une requête est envoyée, le module CORS est systématiquement utilisé. 
-app.use(cors());
-
 
 //IMPORT DE : Routes 
 const userRoutes = require("./routes/user");
@@ -18,6 +15,11 @@ const bodyParser = require('body-parser');
 //IMPORT DE : Mongoose
 const mongoose = require('mongoose');
 
+//Lorsque une requête est envoyée, le module CORS est systématiquement utilisé. 
+app.use(cors());
+
+
+
 //Connection to MongoDB Database 
 mongoose.connect('mongodb+srv://clement:clem@piquantee.nl0h7sg.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -25,32 +27,17 @@ mongoose.connect('mongodb+srv://clement:clem@piquantee.nl0h7sg.mongodb.net/?retr
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch((error) => console.log(error + 'Connexion à MongoDB échouée !'));
 
-
-
-// app.use((req, res, next) => {
-//   console.log('Requête reçue !')
-//   next()
-// });
-
-// app.use((req, res, next) => {
-//     res.status(201)
-//     console.log("Premier middleware")
-//     res.json({message: "Voici la réponse serveur"})
-//     next()
-//   }
-// )
-
+//Utilisation de body parser
 app.use(bodyParser.json());
 
 //LA ROUTE D'AUTHENTIFICATION
 app.use('/api/auth/', userRoutes);
 
 //LA ROUTE DE LA FICHE SAUCE
-app.use('/api/' , FicheSauceRoutes);
+app.use('/api/', FicheSauceRoutes);
 
-//Utilisation de l'application static afin de gérer l'enregistrement des fichiers aux formats image
+//Import et Utilisation de path (ainsi que express.static) afin de gérer l'enregistrement des fichiers statiques aux formats image
 const path = require('path')
-
 app.use('/image', express.static(path.join(__dirname, 'image')));
 
 //EXPORTATION DU MODULE 
